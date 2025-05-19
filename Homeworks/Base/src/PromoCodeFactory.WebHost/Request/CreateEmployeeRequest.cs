af4d2b0e5692d26c5615e1634c3f75079dc87104
KennyDew <dubrovskiynv@gmail.com>
2025-05-20 00:45:02 +0300

    Добавил метод Create в EmployeesController Создал класс CreateEmployeeRequest

---
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Request/CreateEmployeeRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace PromoCodeFactory.WebHost.Request
+{
+    public class CreateEmployeeRequest
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Email { get; set; }
+
+        public int AppliedPromocodesCount { get; set; }
+    }
+}
