14d2a80ac98b75a3763b77ca498e43acb548c395
KennyDew <dubrovskiynv@gmail.com>
2025-06-01 22:29:12 +0300

    added EfRepository implementing IRepository added EfCore (+Design,Sqlite,Tools) libraries пункт №1

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain;
+using PromoCodeFactory.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T>
+        where T : BaseEntity
+
+    {
+        protected readonly DbContext Context;
+        private readonly DbSet<T> _entitySet;
+
+        public EfRepository(DataContext context)
+        {
+            Context = context;
+            _entitySet = Context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return (await _entitySet.AsNoTracking().ToListAsync()).AsEnumerable();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await Context.Set<T>().FindAsync(id).AsTask();
+        }
+    }
+}
