b1efd91c69cc546c851f0f9dc1d546fe1c3afbf6
KennyDew <dubrovskiynv@gmail.com>
2025-06-08 14:15:07 +0300

    added PreferencesController пункт №6

---
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PreferencesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.Administration;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.DataAccess.Repositories;
+using PromoCodeFactory.WebHost.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromoCodeFactory.WebHost.Controllers
+{
+    /// <summary>
+    /// Предпочтения
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PreferencesController : ControllerBase
+    {
+        private readonly IRepository<Preference> _preferenceRepository;
+
+        public PreferencesController(IRepository<Preference> preferenceRepository)
+        {
+            _preferenceRepository = preferenceRepository;
+        }
+
+        /// <summary>
+        /// Получить список предпочтений
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<List<PreferenceResponse>> GetPreferencesAsync()
+        {
+            var preferences = await _preferenceRepository.GetAllAsync() ?? new List<Preference>();
+
+            var preferencesResponseList = preferences.Select(x =>
+                new PreferenceResponse()
+                {
+                    Name = x.Name,
+                }).ToList();
+
+            return preferencesResponseList;
+        }
+    }
+}
