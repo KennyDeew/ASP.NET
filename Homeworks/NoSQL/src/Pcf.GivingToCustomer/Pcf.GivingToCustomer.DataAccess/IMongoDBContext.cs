c8c6b939edf68ee0f24c4025d4e69cc016cc4f4d
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 17:35:35 +0300

    added MongoDBContext

---
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.DataAccess/IMongoDBContext.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+
+namespace Pcf.GivingToCustomer.DataAccess
+{
+    public interface IMongoDBContext
+    {
+        IMongoDatabase Database { get; }
+        IMongoCollection<T> GetCollection<T>(string name = null);
+    }
+}
---
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.DataAccess/Settings/MongoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Pcf.GivingToCustomer.DataAccess.Settings
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
