30c1c2d27fc0671521c5018cd2f7055152f0dfc9
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 22:19:42 +0300

    added Domain's Properies for MongoDb

--- Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.Core/Domain/Customer.cs
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.Core/Domain/Customer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
 
 namespace Pcf.GivingToCustomer.Core.Domain
 {
@@ -7,13 +9,19 @@ namespace Pcf.GivingToCustomer.Core.Domain
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string FullName => $"{FirstName} {LastName}";
-
         public string Email { get; set; }
 
+        // связь многие-ко-многим как лист ID
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public List<Guid> PreferenceIds { get; set; } = new List<Guid>();
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public List<Guid> PromoCodeIds { get; set; } = new List<Guid>();
+
+        [BsonIgnore]
         public virtual ICollection<CustomerPreference> Preferences { get; set; }
-        
+        [BsonIgnore]
         public virtual ICollection<PromoCodeCustomer> PromoCodes { get; set; }
     }
 }
