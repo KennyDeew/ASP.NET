50f7572ed4e46557a23734ad4e22f6121ce20d0f
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 22:21:39 +0300

    added MongoDbInitializer

---
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.DataAccess/Data/MongoDbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using Pcf.GivingToCustomer.Core.Domain;
+using System.Linq;
+
+namespace Pcf.GivingToCustomer.DataAccess.Data
+{
+    public class MongoDbInitializer : IDbInitializer
+    {
+        private readonly IMongoDBContext _context;
+
+        public MongoDbInitializer(IMongoDBContext context)
+        {
+            _context = context;
+        }
+
+        public void InitializeDb()
+        {
+            // чистим БД
+            _context.Database.DropCollection(nameof(Preference) + "s");
+            _context.Database.DropCollection(nameof(Customer) + "s");
+
+            // Добавляем Preferences
+            var preferencesCollection = _context.GetCollection<Preference>();
+            preferencesCollection.InsertMany(FakeDataFactory.Preferences);
+
+            // Добавляем Customers с вложенными Preferences
+            var customersCollection = _context.GetCollection<Customer>();
+            var customers = FakeDataFactory.Customers.Select(c =>
+            {
+                // В Mongo мы обычно не храним навигационные свойства,
+                // поэтому маппим Preferences -> список PreferenceIds
+                c.PreferenceIds = c.Preferences?.Select(p => p.PreferenceId).ToList();
+                c.Preferences = null; // навигационные свойства игнорируем
+                return c;
+            }).ToList();
+
+            customersCollection.InsertMany(customers);
+        }
+    }
+}
