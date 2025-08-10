6c550153daab5a4f6d1664c66acfb907a2bba2ae
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 22:16:10 +0300

    added MongoRepository

---
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.DataAccess/Repositories/MongoRepository.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Driver;
+using Pcf.GivingToCustomer.Core.Abstractions.Repositories;
+using Pcf.GivingToCustomer.Core.Domain;
+using Pcf.GivingToCustomer.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+public class MongoRepository<T> : IRepository<T> where T : BaseEntity
+{
+    private readonly IMongoCollection<T> _collection;
+
+    public MongoRepository(IMongoDBContext context)
+    {
+        _collection = context.GetCollection<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _collection.Find(_ => true).ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(Guid id)
+    {
+        var filter = Builders<T>.Filter.Eq(e => e.Id, id);
+        return await _collection.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids)
+    {
+        var filter = Builders<T>.Filter.In(e => e.Id, ids);
+        return await _collection.Find(filter).ToListAsync();
+    }
+
+    public async Task<T> GetFirstWhere(Expression<Func<T, bool>> predicate)
+    {
+        var filter = Builders<T>.Filter.Where(predicate);
+        return await _collection.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate)
+    {
+        var filter = Builders<T>.Filter.Where(predicate);
+        return await _collection.Find(filter).ToListAsync();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _collection.InsertOneAsync(entity);
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        var filter = Builders<T>.Filter.Eq(e => e.Id, entity.Id);
+        await _collection.ReplaceOneAsync(filter, entity);
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        var filter = Builders<T>.Filter.Eq(e => e.Id, entity.Id);
+        await _collection.DeleteOneAsync(filter);
+    }
+}
