1995a51a4df03e470a70ade906a8d7f534223e02
KennyDew <dubrovskiynv@gmail.com>
2025-09-28 22:20:46 +0300

    Реализовал 1й пункт ДЗ Если партнер не найден, то получаем ошибку 404.

--- Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ Homeworks/UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,47 @@
-﻿namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Controllers;
+using PromoCodeFactory.WebHost.Models;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
     public class SetPartnerPromoCodeLimitAsyncTests
     {
         //TODO: Add Unit Tests
+        private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+        private readonly PartnersController _partnersController;
+        private readonly Fixture _fixture;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public SetPartnerPromoCodeLimitAsyncTests()
+        {
+            _partnersRepositoryMock = new Mock<IRepository<Partner>>();
+            _partnersController = new PartnersController(_partnersRepositoryMock.Object);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async Task SetPartnerPromoCodeLimitAsync_Should_Throw_Exception_If_Partner_Is_NotFound_Return_404()
+        {
+            // Arrange
+            var partnerId = Guid.NewGuid();
+            var request = _fixture.Create<SetPartnerPromoCodeLimitRequest>();
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
     }
 }
